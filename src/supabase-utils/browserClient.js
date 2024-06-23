c4c6ddabb3fbff7a1877fb3cf3cdff0e29db0f17
David Lorenz <info@activenode.de>
2024-06-23 20:48:53 +0200

    fix

--- src/supabase-utils/browserClient.js
+++ src/supabase-utils/browserClient.js
@@ -1,7 +1,12 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const getSupabaseBrowserClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+export const getSupabaseBrowserClient = () => {
+  try {
+    return createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
