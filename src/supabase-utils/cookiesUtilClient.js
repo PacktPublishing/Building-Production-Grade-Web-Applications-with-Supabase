52aeecc3ee1449db1df8b1334e7f1143dae4bc66
David Lorenz <info@activenode.de>
2024-06-23 20:50:18 +0200

    fix

--- src/supabase-utils/cookiesUtilClient.js
+++ src/supabase-utils/cookiesUtilClient.js
@@ -2,16 +2,21 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const getSupabaseCookiesUtilClient = () => {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
-        get: (name) => cookies().get(name)?.value,
-        set: (name, value, options) =>
-          cookies().set({ name, value, ...options }),
-        remove: (name, options) =>
-          cookies().set({ name, value: "", ...options }),
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookies) {
+          cookies.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options);
+          });
+        },
       },
     }
   );
