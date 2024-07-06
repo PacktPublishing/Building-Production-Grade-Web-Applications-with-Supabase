f7695fa440bd6be2cb64383924fa7bb27a4b76b0
David Lorenz <info@activenode.de>
2024-07-06 22:46:39 +0200

    fix: mw

--- src/middleware.js
+++ src/middleware.js
@@ -4,25 +4,27 @@ import { TENANT_MAP } from "./tenant-map";
 import { buildUrl, getHostnameAndPort } from "./utils/url-helpers";
 
 export async function middleware(req) {
-  const res = NextResponse.next();
-
-  const supabase = getSupabaseReqResClient({ req, res });
+  const { supabase } = getSupabaseReqResClient({ request: req });
   const session = await supabase.auth.getSession();
 
   const [hostname] = getHostnameAndPort(req);
 
   if (hostname in TENANT_MAP === false) {
-    return NextResponse.error();
+    return NextResponse.rewrite(new URL("/not-found", req.url));
   }
 
   const requestedPath = req.nextUrl.pathname;
   const sessionUser = session.data?.session?.user;
 
+  // old way of parsing tenant from path
+  // const [tenant, ...restOfPath] = requestedPath.substr(1).split("/");
+  // const applicationPath = "/" + restOfPath.join("/");
+
   const tenant = TENANT_MAP[hostname];
   const applicationPath = requestedPath;
 
   if (!/[a-z0-9-_]+/.test(tenant)) {
-    return NextResponse.error();
+    return NextResponse.rewrite(new URL("/not-found", req.url));
   }
 
   if (applicationPath.startsWith("/tickets")) {
